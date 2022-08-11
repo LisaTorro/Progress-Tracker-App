9e4ea3dd47fcc47e32a32cb6fad88e0f57fda52a
ThomasJensWilliams <109638577+ThomasJensWilliams@users.noreply.github.com>
2022-08-11 15:58:30 -0700

    Delete Progress-Tracker-App/ProgressTracker directory

--- Progress-Tracker-App/ProgressTracker/App.java
+++
@@ -1,9 +0,0 @@
-// Reviewed on 08/03/2022 at 9:45AM
-
-package ProgressTracker;
-
-public class App {
-    public static void main(String [] args) throws Exception {
-        new MyFrame();
-    }
-}
